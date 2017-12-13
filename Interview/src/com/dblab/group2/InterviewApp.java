565db257934d93c3dc010a1f9da21a95720fe403
David Paul Brackin <dev.mintgames@gmail.com>
2017-12-13 20:15:02 +0800

    finished main menu, please implement

--- Interview/src/com/dblab/group2/InterviewApp.java
+++ Interview/src/com/dblab/group2/InterviewApp.java
@@ -6,30 +6,55 @@ import java.sql.SQLException;
 
 public class InterviewApp {
 	
-	final String DBIP = "54.169.15.228";
-	final String DBURL = "jdbc:mysql://" + DBIP + ":3306/Group2";
-	final String USER = "Group2";
-	final String PASSWORD  = "mybirthday";
+	private final String DBIP = "54.169.15.228";
+	private final String DBURL = "jdbc:mysql://" + DBIP + ":3306/Group2";
+	private final String USER = "Group2";
+	private final String PASSWORD  = "mybirthday";
+	
+	private Connection con;
 	
 	public InterviewApp(){
 		
-		connectToDb();
+		try {
+			connectToDb();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public void connectToDb(){
+	public void connectToDb() throws SQLException{
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection(DBURL,USER,PASSWORD);
+			con = DriverManager.getConnection(DBURL,USER,PASSWORD);
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
 		InterviewApp app = new InterviewApp();
+	}
+	
+	public static void displayMenu() {
+		System.out.println("-------Read-------");
+		System.out.println("<1> Show Interviewers");
+		System.out.println("<2> Show Interview Schedules");
+		System.out.println("<3> Show Applicants");
+		System.out.println("------Create------");
+		System.out.println("<4> Add new Interviewer");
+		System.out.println("<5> Add new Interview Schedule");
+		System.out.println("<6> Add new Applicant");
+		System.out.println("------Update------");
+		System.out.println("<7> Edit Interviewer Info");
+		System.out.println("<8> Edit Interview Schedule");
+		System.out.println("<9> Edit Applicant Info");
+		System.out.println("------Delete-----");
+		System.out.println("<10> Remove Interviewer");
+		System.out.println("<11> Remove Interview Schedule");
+		System.out.println("<12> Remove Applicant");
+		System.out.println("<0> Exit ");
 	}
 
 }
